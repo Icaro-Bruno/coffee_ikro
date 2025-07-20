889035779b34f98d7ad23b9d5cc86c82dff1a4c9
_ikro_ <icaroclash77@gmail.com>
2025-07-20 00:01:04 -0300

    admin
    adicionei criptografia com "BCryptPasswordEncoder" a pagina de login do admin

--- src/main/java/com/restaurante/restaurante/service/AdminService.java
+++ src/main/java/com/restaurante/restaurante/service/AdminService.java
@@ -3,6 +3,7 @@ package com.restaurante.restaurante.service;
 import com.restaurante.restaurante.model.AdminModel;
 import com.restaurante.restaurante.repository.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,8 +14,16 @@ public class AdminService {
     @Autowired
     private AdminRepository adminRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     public boolean autenticar(String usuario, String senha) {
         Optional<AdminModel> buscaAdmin = adminRepository.findByUsuario(usuario);
-        return buscaAdmin.isPresent() && buscaAdmin.get().getSenha().equals(senha);
+        return buscaAdmin.isPresent() && passwordEncoder.matches(senha, buscaAdmin.get().getSenha());
+    }
+
+    public AdminModel salvar(AdminModel admin) {
+        admin.setSenha(passwordEncoder.encode(admin.getSenha()));
+        return adminRepository.save(admin);
     }
 }
